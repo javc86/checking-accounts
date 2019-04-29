8e16606d5d0f4ff4112cb9ebd7cb8cfff8e03086
Johander Vazquez <johander.vazquez@gmail.com>
2019-04-29 20:42:27 -0300

    Added basic structure with webpack and nodejs

---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import webpack from 'webpack';
+import middleware from 'webpack-dev-middleware';
+import webpackConfig from '../../webpack.config';
+import path from 'path';
+
+const app = express();
+app.use(middleware(webpack(webpackConfig)));
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, '../../public')));
+
+app.listen(4800, () => {
+    console.log('SERVER RUNNING ON THE 4500 PORT')
+});
