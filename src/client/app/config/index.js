6e45cb3b502c4c96d2d5e0db6bb050893b525d43
Johander Vazquez <johander.vazquez@gmail.com>
2019-05-06 17:40:47 -0300

    Minor fix

--- src/client/app/config/index.js
+++ src/client/app/config/index.js
@@ -3,5 +3,5 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    url: process.env.APP_ENV === 'dev' ? 'http://localhost:4800/api' : 'ec2-18-219-247-19.us-east-2.compute.amazonaws.com:4800/api'
+    url: process.env.APP_ENV === 'dev' ? 'http://localhost:4800/api' : 'http://ec2-18-219-247-19.us-east-2.compute.amazonaws.com:4800/api'
 }
